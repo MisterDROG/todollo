9dab29592160c86c0179f9dea2605cf1d970234b
MrDrog <fireman3085@mail.ru>
2023-05-04 20:00:14 +0300

    feature: deploy to git hub

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,7 +5,7 @@ function Header() {
     return (
         <div className="header">
             <div className="header__logo-container">
-                <img className="header__logo-img" src={todolloLogo} alt='ToDollo Logo'></img>
+                <img className="header__logo-img" src={todolloLogo} alt='ToDollo Logo' />
                 <p className="header__logo-text">ToDollo board</p>
             </div>
         </div>
