bbd5852836f40b70bc9ac6d9b4333e6e6d05c52f
MrDrog <fireman3085@mail.ru>
2023-08-28 16:11:34 +0300

    fix: updated jest tests for Card component 2

--- src/redux/middlewares/logger.ts
+++ src/redux/middlewares/logger.ts
@@ -3,13 +3,13 @@ import { Middleware } from "redux";
 //middleware for logging state actions (analog of Redux DevTools)
 
 export const loggerMiddleware: Middleware = (store) => (next) => (action) => {
-    // console.log('action', action)
+    console.log('action', action)
 
-    // console.log('before', store.getState())
+    console.log('before', store.getState())
 
     let result = next(action)
 
-    // console.log('after', store.getState())
+    console.log('after', store.getState())
 
     return result
 }
